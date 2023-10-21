63df38bb5c724afc25634f5f2aa6f093057e58f3
zhiweiwu <tonywuzhiwei@hotmail.com>
2023-10-21 21:31:17 +1100

    upload main function
    upload folder contain main function

---
+++ transactionCost/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using transactionCost.Model;
+
+
+namespace transactionCost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = File.ReadAllText("../json/transactions.json");
+            List<Transaction> transactions = JsonConvert.DeserializeObject<List<Transaction>>(json);
+
+            foreach (var transaction in transactions)
+            {
+                int transactionId = transaction.TransactionId;
+                double totalCost = TicketPricingCalculator.CalculateTotalCost(transaction.Customers);
+
+                Console.WriteLine($"## Transaction {transactionId} ##");
+                Console.WriteLine($"Projected total cost: ${totalCost:F2}\n");
+            }
+        }
+
+
+    }
+}
---
+++ transactionCost/TicketPricingCalculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using transactionCost.Model;
+
+namespace transactionCost
+{
+    public class TicketPricingCalculator
+    {
+        public static double CalculateTotalCost(List<Customer> customers)
+        {
+            double totalCost = 0;
+            int childCount = 0;
+
+            foreach (var customer in customers)
+            {
+                if (customer.Age >= 65)
+                {
+                    totalCost += 0.7 * 25;
+                }
+                else if (customer.Age >= 18)
+                {
+                    totalCost += 25;
+                }
+                else if (customer.Age >= 11)
+                {
+                    totalCost += 12;
+                }
+                else if (customer.Age < 0) {
+                    totalCost = -1;
+                    Console.WriteLine("Error: Age cannot be less than 0 for customer: " + customer.Name);
+                    break;
+
+                }
+                else
+                {
+                    totalCost += 5;
+                    childCount++;
+                    if (childCount >= 3)
+                    {
+                        totalCost -= childCount * 5 * 0.25;
+                    }
+                }
+            }
+
+            return totalCost;
+        }
+    }
+}
